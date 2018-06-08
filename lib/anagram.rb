e9e772357802e80014955d4f446b20c0452cb51b
github username <github email address>
2018-06-08 16:32:51 -0400

    dan brown eat your heart out

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+#-------Coded by awchang12 and wley3337-----------
+
+
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split(%r{}).sort == self.word.split(%r{}).sort
+    end
+  end
+
+end
